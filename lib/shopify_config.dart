ce5a618766c3971838c96719a75f5b1e67a77ca6
Sebastian Hennig <hello@bastihennig.com>
2020-11-30 21:30:59 +0100

    Add language as config option

--- lib/shopify_config.dart
+++ lib/shopify_config.dart
@@ -1,7 +1,6 @@
 import 'package:graphql/client.dart';
 
 class ShopifyConfig {
-
   /// Your own unique access key found on your Shopify dashboard under apps -> manage private apps -> your-app-name .
   static String _storefrontAccessToken;
 
@@ -15,21 +14,31 @@ class ShopifyConfig {
   /// Default is set to 2020-04.
   static String _storefrontApiVersion = '2020-04';
 
+  /// The language of the content sent by the Storefront API
+  ///
+  /// eg: "de"
+  static String _storeLanguage;
+
   /// Sets the config.
   ///
   /// IMPORTANT: preferably call this inside the main function or at least before instantiating other Shopify classes.
-  static void setConfig(String storefrontAccessToken, String storeUrl, String storefrontApiVersion){
+  static void setConfig(String storefrontAccessToken, String storeUrl,
+      String storefrontApiVersion,
+      {String storeLanguage}) {
     _storefrontAccessToken ??= storefrontAccessToken;
     _storeUrl ??= storeUrl;
-    _storefrontApiVersion = storefrontApiVersion != null ? storefrontApiVersion : _storefrontApiVersion;
+    _storefrontApiVersion = storefrontApiVersion != null
+        ? storefrontApiVersion
+        : _storefrontApiVersion;
+    _storeLanguage ??= storeLanguage;
   }
 
   /// The GraphQlClient used for communication with the Storefront API.
   static final GraphQLClient graphQLClient = GraphQLClient(
-    link: HttpLink(
-        headers: {'X-Shopify-Storefront-Access-Token':'$_storefrontAccessToken'},
-        uri: 'https://$_storeUrl/api/$_storefrontApiVersion/graphql.json'
-    ),
+    link: HttpLink(headers: {
+      'X-Shopify-Storefront-Access-Token': '$_storefrontAccessToken',
+      'Accept-Language': '$_storeLanguage',
+    }, uri: 'https://$_storeUrl/api/$_storefrontApiVersion/graphql.json'),
     cache: NormalizedInMemoryCache(
       dataIdFromObject: typenameDataIdFromObject,
     ),
